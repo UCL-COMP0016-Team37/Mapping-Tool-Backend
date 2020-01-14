e03d56485a1f622b483e705f52c768364ef6c28c
Yangtao Ge <geyangtao@yahoo.com>
2020-01-14 12:56:02 +0000

    Init

---
+++ src/main/java/uk/ac/ucl/mappingtool/MappingToolApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.mappingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/ucl/mappingtool/MappingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.mappingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
