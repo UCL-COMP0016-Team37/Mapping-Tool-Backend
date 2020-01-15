2c33496ef84da7de5967530ddd114b5ef7ce3a2a
Yangtao Ge <geyangtao@yahoo.com>
2020-01-15 16:49:02 +0000

    add CORS Config

---
+++ src/main/java/uk/ac/ucl/mappingtool/v1/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package uk.ac.ucl.mappingtool.v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .maxAge(3600);
+    }
+}
