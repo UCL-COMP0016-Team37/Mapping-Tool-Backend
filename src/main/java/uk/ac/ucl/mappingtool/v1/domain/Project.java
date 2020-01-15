e080f1ac20c110616f671b78c8d890d280f9efc7
Yangtao Ge <geyangtao@yahoo.com>
2020-01-15 15:49:51 +0000

    correct for one project

--- src/main/java/uk/ac/ucl/mappingtool/v1/domain/Project.java
+++ src/main/java/uk/ac/ucl/mappingtool/v1/domain/Project.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -15,7 +16,6 @@ import javax.persistence.Table;
 @Entity
 @ApiModel(description = "Project Model")
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "projects")
 public class Project {
 
@@ -23,38 +23,71 @@ public class Project {
     @ApiModelProperty("Project Unique ID")
     private String interaction_intervention_id;
 
+    @Column(nullable = false)
     private String organization;
+    @Column()
     private String org_intervention_id;
+    @Column()
     private String project_tags;
+    @Column(nullable = false)
     private String project_name;
+    @Column()
     private String project_description;
+    @Column()
     private String activities;
+    @Column()
     private String additional_information;
+    @Column()
     private String start_date;
+    @Column()
     private String end_date;
+    @Column()
     private String sectors;
+    @Column()
     private String cross_cutting_issues;
+    @Column()
     private String budget_numeric;
+    @Column()
     private String budget_currency;
+    @Column()
     private String budget_value_date;
+    @Column()
     private String international_partners;
+    @Column()
     private String local_partners;
+    @Column()
     private String prime_awardee;
+    @Column()
     private String target_project_reach;
+    @Column()
     private String actual_project_reach;
+    @Column()
     private String project_reach_unit;
+    @Column()
     private String target_groups;
+    @Column()
     private String geographic_scope;
+    @Column(nullable = false)
     private String location;
+    @Column()
     private String project_contact_person;
+    @Column()
     private String project_contact_position;
+    @Column()
     private String project_contact_email;
+    @Column()
     private String project_contact_phone_number;
+    @Column()
     private String project_website;
+    @Column()
     private String date_provided;
+    @Column()
     private String date_updated;
+    @Column(nullable = false)
     private String status;
+    @Column()
     private String donors;
+    @Column(nullable = false)
     private String humanitarian;
 
 
