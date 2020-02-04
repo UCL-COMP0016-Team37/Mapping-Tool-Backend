fec53f564812a246865f75c4366549dca24f6d5f
Yangtao Ge <geyangtao@yahoo.com>
2020-02-04 16:02:30 +0000

    debug location attributes

---
+++ src/main/java/uk/ac/ucl/mappingtool/v2/domain/activity/location/Administrative.java
@@ -0,0 +1,17 @@
+package uk.ac.ucl.mappingtool.v2.domain.activity.location;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.ac.ucl.mappingtool.v2.domain.activity.vocabulary.Vocabulary;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Administrative {
+    private Integer id;
+    private String code;
+    private Vocabulary vocabulary;
+    private Integer level;
+}
