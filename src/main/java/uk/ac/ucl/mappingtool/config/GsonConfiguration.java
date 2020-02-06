1de0f24503eb156b3e22aed117d1c1aea5c81884
Yangtao Ge <geyangtao@yahoo.com>
2020-02-06 18:25:53 +0000

    fix swagger problem

--- src/main/java/uk/ac/ucl/mappingtool/config/GsonConfiguration.java
+++ src/main/java/uk/ac/ucl/mappingtool/config/GsonConfiguration.java
@@ -1,21 +1,31 @@
 package uk.ac.ucl.mappingtool.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.spring.web.json.Json;
 
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
-public class GsonConfiguration extends WebMvcConfigurerAdapter {
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter); // 删除MappingJackson2HttpMessageConverter
-        converters.add(new GsonHttpMessageConverter()); // 添加GsonHttpMessageConverter
+public class GsonConfiguration {
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter() {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson());
+        return converter;
+    }
+
+    private Gson gson() {
+        final GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
+        return builder.create();
     }
 
 }
