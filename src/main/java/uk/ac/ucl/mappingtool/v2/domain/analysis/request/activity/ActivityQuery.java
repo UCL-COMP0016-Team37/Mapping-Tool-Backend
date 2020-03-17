bb5cd23ca47fdc8b8c5a74075017822763abc4f4
Yangtao Ge <geyangtao@yahoo.com>
2020-03-17 19:47:00 +0000

    add top org test

---
+++ src/main/java/uk/ac/ucl/mappingtool/v2/domain/analysis/request/activity/ActivityQuery.java
@@ -0,0 +1,16 @@
+package uk.ac.ucl.mappingtool.v2.domain.analysis.request.activity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActivityQuery<T>{
+    private Integer count;
+    private List<ActivityQueryItem<T>> results;
+}
